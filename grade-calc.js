082111d822fd58b432fa137b7f881108be5143a0
Arslan <malik.arsalan105@gmail.com>
2020-04-05 12:24:36 -0700

    if condition for grade according to percentage

--- grade-calc.js
+++ grade-calc.js
@@ -1,6 +1,18 @@
 let calc =function(score ,totalscore){
     let percent =(score/totalscore)*100;
     
+if (percent>=90){
+    return `You got a A ${percent}%!`;
+}
+else if(percent>=80){
+    return `You got a B ${percent}%!`;
+}else if(percent>=70){
+    return `You got a C ${percent}%!`;
+}else if(percent>=60){
+    return `You got a D ${percent}%!`;
+}else if(percent>=0){
+    return `You got a F ${percent}%!`;
+}
 
 }
 let result = calc(18,20);
